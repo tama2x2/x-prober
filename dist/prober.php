4b3ca56e4d048223cdb3a4a9e04fa0ee047a2f72
tama2x2 <104321579+tama2x2@users.noreply.github.com>
2022-04-25 03:49:09 +0900

    Update prober.php

--- dist/prober.php
+++ dist/prober.php
@@ -10,7 +10,7 @@ exit; } } namespace InnStudio\Prober\Components\MyInfo; use InnStudio\Prober\Com
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="renderer" content="webkit">
-<title>{$appName} v{$version}</title>
+<title>{$appName} v{$version}+69</title>
 <script>window.CONF = {$scriptConf};</script>
 <script src="{$scriptUrl}" async></script>
 </head>
